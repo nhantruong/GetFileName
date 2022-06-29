e2fb3f9aa205fb019a433bbe1fde0f0736387542
NhanTruong <truongchinhan2010@gmail.com>
2022-06-29 18:32:10 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace GetFileName
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.ProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSelectDirectory = new System.Windows.Forms.Button();
+            this.lblSelectedPath = new System.Windows.Forms.LinkLabel();
+            this.treeView_Directory = new System.Windows.Forms.TreeView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.treeView_FileInDirectory = new System.Windows.Forms.TreeView();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus,
+            this.ProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 543);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(655, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // ProgressBar1
+            // 
+            this.ProgressBar1.Name = "ProgressBar1";
+            this.ProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblSelectedPath);
+            this.groupBox1.Controls.Add(this.btnSelectDirectory);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(655, 77);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chọn Thư mục";
+            // 
+            // btnSelectDirectory
+            // 
+            this.btnSelectDirectory.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnSelectDirectory.Location = new System.Drawing.Point(3, 16);
+            this.btnSelectDirectory.Name = "btnSelectDirectory";
+            this.btnSelectDirectory.Size = new System.Drawing.Size(96, 58);
+            this.btnSelectDirectory.TabIndex = 0;
+            this.btnSelectDirectory.Text = "&Chọn thư mục";
+            this.btnSelectDirectory.UseVisualStyleBackColor = true;
+            this.btnSelectDirectory.Click += new System.EventHandler(this.btnSelectDirectory_Click);
+            // 
+            // lblSelectedPath
+            // 
+            this.lblSelectedPath.AutoSize = true;
+            this.lblSelectedPath.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblSelectedPath.Font = new System.Drawing.Font("Lexend Deca", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSelectedPath.Location = new System.Drawing.Point(99, 50);
+            this.lblSelectedPath.Name = "lblSelectedPath";
+            this.lblSelectedPath.Size = new System.Drawing.Size(170, 24);
+            this.lblSelectedPath.TabIndex = 2;
+            this.lblSelectedPath.TabStop = true;
+            this.lblSelectedPath.Text = "Folder Path selected...";
+            // 
+            // treeView_Directory
+            // 
+            this.treeView_Directory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView_Directory.Location = new System.Drawing.Point(0, 0);
+            this.treeView_Directory.Name = "treeView_Directory";
+            this.treeView_Directory.Size = new System.Drawing.Size(229, 466);
+            this.treeView_Directory.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 77);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView_Directory);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.treeView_FileInDirectory);
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(655, 466);
+            this.splitContainer1.SplitterDistance = 229;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(143, 17);
+            this.lblStatus.Text = "Chọn thư mục để bắt đầu";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnExport);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(0, 405);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(422, 61);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Actions";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnExport.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExport.Location = new System.Drawing.Point(3, 16);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(160, 42);
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "Xuất file được chọn qua Excel";
+            this.btnExport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnExport.UseVisualStyleBackColor = true;
+            // 
+            // treeView_FileInDirectory
+            // 
+            this.treeView_FileInDirectory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView_FileInDirectory.Location = new System.Drawing.Point(0, 0);
+            this.treeView_FileInDirectory.Name = "treeView_FileInDirectory";
+            this.treeView_FileInDirectory.Size = new System.Drawing.Size(422, 405);
+            this.treeView_FileInDirectory.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 565);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Export files";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar ProgressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.LinkLabel lblSelectedPath;
+        private System.Windows.Forms.Button btnSelectDirectory;
+        private System.Windows.Forms.TreeView treeView_Directory;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.TreeView treeView_FileInDirectory;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetFileName
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectDirectory_Click(object sender, EventArgs e)
+        {
+            //ProgressBar1.Maximum = 0;
+
+            FolderBrowserDialog fd = new FolderBrowserDialog();
+            fd.ShowDialog(Parent);
+            string selectesDir = fd.SelectedPath;
+
+            lblSelectedPath.Text = "";
+            lblSelectedPath.Text = selectesDir;
+
+            //Thư mục cha
+            DirectoryInfo di = new DirectoryInfo(selectesDir);
+            if (di.Exists)
+            {
+                string Root = di.Name;
+
+                List<string> fileInDir = new List<string>();
+                fileInDir = FindingFileInDir(selectesDir);
+
+                if (fileInDir.Count() != 0 || fileInDir != null)
+                {
+                    ProgressBar1.Value = 0;
+                    lblStatus.Text = "";
+                    lblStatus.Text = $"Có tổng cộng {fileInDir.Count()} file trong thư mục này";
+                    AddFilesToTreeView(Root, fileInDir, treeView_Directory);
+                    treeView_Directory.ExpandAll();
+                }
+                else
+                {
+                    ProgressBar1.Value = 0;
+                    lblStatus.Text = "";
+                    lblStatus.Text = "Thư mục rỗng";
+                }
+            }
+
+
+        }
+
+        private void AddFilesToTreeView(string root, List<string> fileInDir, TreeView treeView_Directory)
+        {
+            TreeNode _root = new TreeNode(root);
+            treeView_Directory.Nodes.Add(root);
+
+            foreach (string item in fileInDir)
+            {
+                FileInfo fi = new FileInfo(item);
+                
+
+                TreeNode itm = new TreeNode(item);
+                _root.Nodes.Add(itm);
+            }
+        }
+
+        private List<string> FindingFileInDir(string selectesDir)
+        {
+            if (string.IsNullOrEmpty(selectesDir)) return null;
+            List<string> fileInDir = new List<string>();
+
+            //ProgressBar1.Value = 0;
+            foreach (string item in Directory.GetFiles(selectesDir))
+            {
+                fileInDir.Add(item);
+                //ProgressBar1.Value++;
+            }
+            return fileInDir;
+        }
+    }
+}
